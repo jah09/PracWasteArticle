6f6f865604577f51b85ada318d88ac8af9db840c
jah09 <jasuhot1@gmail.com>
2023-09-12 08:24:58 +0800

    9-12- Morning Initial- Login screen

---
+++ DailyAgendaProject/app/src/main/java/com/example/dailyagenda/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.dailyagenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
